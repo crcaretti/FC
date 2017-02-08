449416debc31cb37b230fc1753d85c3357e7014e
pregmodfan <pregmodfan@cock.li>
2017-02-08 14:50:14 +0200

    compile_debug result filename changed

--- compile_debug.bat
+++ compile_debug.bat
@@ -6,6 +6,6 @@
 :: Will add all *.tw files to StoryIncludes.
 "%~dp0devTools\python-3.5.3\python.exe" "%~dp0devTools\scripts\includes.py" "src\config\start.tw.proto" "src\config\start.tw" "src" "%*"
 
-CALL "%~dp0devTools\tweeGo\tweego.exe" -o "%~dp0bin/FC.html" "%~dp0src\config\start.tw"
+CALL "%~dp0devTools\tweeGo\tweego.exe" -o "%~dp0bin/FC_pregmod.html" "%~dp0src\config\start.tw"
 ECHO Done
 PAUSE
