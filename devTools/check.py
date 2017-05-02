b4f57dcfeea30783d340f581d4c9502de01af5fa
Stuffed <stuffedgames@gmail.com>
2017-05-02 23:06:37 +0900

    sanityCheck - add check for a variable only used once

--- devTools/check.py
+++ devTools/check.py
@@ -3,8 +3,11 @@ import fileinput
 import re
 import sys
 
+WARNING = '\033[93m'
+ENDC = '\033[0m'
+
 def myprint(*args):
-    print(filename + ":",*args)
+    print(WARNING, filename + ":", ENDC,*args)
 
 def yield_line_and_islastline(f):
     global filename
@@ -26,43 +29,49 @@ pattern = re.compile(r'(<<(\/?) *(if|for|else|switch|case)[^<>]*)')
 
 
 tagfound = []
-for line, isLastLine in yield_line_and_islastline(fileinput.input()):
-    for (whole,end,tag) in re.findall(pattern,line):
-        if tag == "else" or tag == 'case':
-            if len(tagfound) == 0:
-                myprint("Found", tag, "but with no opening tag:")
-                myprint("  ", linenumber,":", whole)
-                fileinput.nextfile()
-            lasttag = tagfound[-1]
-            if (tag == "else" and lasttag["tag"] != "if") or (tag == "case" and lasttag["tag"] != "switch"):
-                myprint("Mismatched else: Opening tag was:")
-                myprint("  ",lasttag["linenumber"],":", lasttag["whole"])
-                myprint("But this tag was:")
-                myprint("  ",linenumber,":", whole)
-                fileinput.nextfile()
-                break
-        elif end != '/':
-            tagfound.append({"whole": whole, "linenumber":linenumber,"tag":tag})
-        else:
-            if len(tagfound) == 0:
-                myprint("Found closing tag but with no opening tag:")
-                myprint("  ", linenumber,":", whole)
-                fileinput.nextfile()
-                break
-            lasttag = tagfound.pop()
-            if lasttag["tag"] != tag:
-                myprint("Mismatched tag: Opening tag was:")
-                myprint("  ",lasttag["linenumber"],":", lasttag["whole"])
-                myprint("Closing tag was:")
-                myprint("  ",linenumber,":", whole)
-                fileinput.nextfile()
-                break
+try:
+    for line, isLastLine in yield_line_and_islastline(fileinput.input()):
+        for (whole,end,tag) in re.findall(pattern,line):
+            if tag == "else" or tag == 'case':
+                if len(tagfound) == 0:
+                    myprint("Found", tag, "but with no opening tag:")
+                    myprint("  ", linenumber,":", whole)
+                    fileinput.nextfile()
+                lasttag = tagfound[-1]
+                if (tag == "else" and lasttag["tag"] != "if") or (tag == "case" and lasttag["tag"] != "switch"):
+                    myprint("Mismatched else: Opening tag was:")
+                    myprint("  ",lasttag["linenumber"],":", lasttag["whole"])
+                    myprint("But this tag was:")
+                    myprint("  ",linenumber,":", whole)
+                    fileinput.nextfile()
+                    break
+            elif end != '/':
+                tagfound.append({"whole": whole, "linenumber":linenumber,"tag":tag})
+            else:
+                if len(tagfound) == 0:
+                    myprint("Found closing tag but with no opening tag:")
+                    myprint("  ", linenumber,":", whole)
+                    fileinput.nextfile()
+                    break
+                lasttag = tagfound.pop()
+                if lasttag["tag"] != tag:
+                    myprint("Mismatched tag: Opening tag was:")
+                    myprint("  ",lasttag["linenumber"],":", lasttag["whole"])
+                    myprint("Closing tag was:")
+                    myprint("  ",linenumber,":", whole)
+                    fileinput.nextfile()
+                    break
 
 
-    if isLastLine:
-        if len(tagfound) != 0:
-            myprint("End of file found but", len(tagfound), ("tag hasn't" if len(tagfound)==1 else "tags haven't"), "been closed:")
-        for tag in tagfound:
-            myprint("  ", tag["linenumber"],":", tag["whole"])
-        tagfound = []
+        if isLastLine:
+            if len(tagfound) != 0:
+                myprint("End of file found but", len(tagfound), ("tag hasn't" if len(tagfound)==1 else "tags haven't"), "been closed:")
+            for tag in tagfound:
+                myprint("  ", tag["linenumber"],":", tag["whole"])
+            tagfound = []
+except UnicodeDecodeError as e:
+    myprint(e)
+    print("   Hint: In linux, you can get more details about unicode errors by running:")
+    print("     isutf8", filename) 
+
 
