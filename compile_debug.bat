80e54f4733deb1107b72d267bb5440f2417be1e4
Spaghetti Code <spaghetticode@cock.li>
2017-09-16 00:30:07 -0700

    TweeGo v1.0.1 And Associated Changes

--- compile_debug.bat
+++ compile_debug.bat
@@ -1,19 +1,11 @@
 @echo off
 :: Free Cities Basic Compiler - Windows
 
-:: Will add all *.tw files to StoryIncludes.
-del src\config\start.tw
-copy src\config\start.tw.proto start.tw.tmp >nul
->>start.tw.tmp (for /r "src" %%F in (*.tw) do echo %%F)
-move start.tw.tmp src\config\start.tw >nul
+:: Set working directory
+pushd %~dp0
 
-:: Run the appropriate compiler for the user's CPU architecture.
-if %PROCESSOR_ARCHITECTURE% == AMD64 (
-    CALL "%~dp0devTools\tweeGo\tweego_win64.exe" -o "%~dp0bin/FC_pregmod.html" "%~dp0src\config\start.tw"
-) else (
-    CALL "%~dp0devTools\tweeGo\tweego_win86.exe" -o "%~dp0bin/FC_pregmod.html" "%~dp0src\config\start.tw"
-)
+:: Compile the game
+call "%~dp0compile.bat"
 
-del src\config\start.tw
-ECHO Done
+popd
 PAUSE
