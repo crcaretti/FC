797ff7c5225845e1fcc7ca988e33c55040383282
j <okp57855@psoxs.com>
2018-02-03 16:23:13 +1300

    Reworked.

--- devTools/AutoGitVersionUpload.sh
+++ devTools/AutoGitVersionUpload.sh
@@ -1,34 +1,17 @@
-## 	Requires MEGAcmd, git, mv, cd, mkidr, echo
 #!/bin/sh
-LocalDir=~/fc-pregmod/
-RemoteDir=FCGIT
-U=anon@anon.anon
-P=13245
-	mega-login $U $P
-if  [[ $1 = "-NewStart" ]]; then
-	echo "Option -NewStart is on"
-	mega-mkdir $RemoteDir
-	mega-export -a $RemoteDir
-	mkdir $LocalDir
-	git clone https://gitgud.io/pregmodfan/fc-pregmod.git $LocalDir
-	cd $LocalDir/
-	AbrevHash=`git log -n1 --abbrev-commit |grep -m1 commit | sed 's/commit //'`
-	FullHash=`git log -n1 |grep -m1 commit | sed 's/commit //'`
-	rm bin/*.html & git pull 
-	./compile-git > /dev/null
-	mv bin/*.html "bin/FC-pregmod-CommitDate-$(date '+%F %H-%M' --utc --date="$(git show '--format=format:%cD' HEAD)").html"
-	mega-put bin/*.html $RemoteDir
-	mega-logout
-	echo "All done."
-else
-	echo "Option -NewStart is off"
-	cd $LocalDir/
-	AbrevHash=`git log -n1 --abbrev-commit |grep -m1 commit | sed 's/commit //'`
-	FullHash=`git log -n1 |grep -m1 commit | sed 's/commit //'`
-	rm bin/*.html & git pull 
-	./compile-git > /dev/null
-	mv bin/*.html "bin/FC-pregmod-CommitDate-$(date '+%F %H-%M' --utc --date="$(git show '--format=format:%cD' HEAD)").html"
-	mega-put bin/*.html $RemoteDir
-	mega-logout
-	echo "All done."
-fi
+#Requires MEGAcmd, git, mv, cd, mkidr, echo
+COUNTER=0 
+while [ $COUNTER -ge 0 ]; do
+LocalDir=~/fc-pregmod/ RemoteDir=FC-GIT U=anon@anon.anon P=13245 && mega-login $U $P
+	if [ $COUNTER -eq 0 ]; then mega-mkdir $RemoteDir && mega-export -a $RemoteDir && mkdir $LocalDir && git clone https://gitgud.io/pregmodfan/fc-pregmod.git $LocalDir
+	fi 
+	 cd $LocalDir/ && AbrevHash=`git log -n1 --abbrev-commit |grep -m1 commit | sed 's/commit //'`
+	if [ $COUNTER -eq 0 ]; then
+		 echo "First run. Compliling, formatting and placing .html." && ./compile > /dev/null && mv bin/FC_pregmod.html "bin/FC-pregmod-$(date '+%F-%H-%M' --utc --date="$(git show '--format=format:%cD' HEAD)")-$AbrevHash.html" && mega-put bin/*.html $RemoteDir && echo "Compiled .html placed."
+	else
+		git pull| if [ "Already up to date" ]; then echo "No updated files, exiting." && mega-logout > /dev/null && exit 
+		else echo "Compliling, formatting and placing .html." && ./compile > /dev/null && mv bin/FC_pregmod.html "bin/FC-pregmod-$(date '+%F-%H-%M' --utc --date="$(git show '--format=format:%cD' HEAD)")-$AbrevHash.html" && mega-put bin/*.html $RemoteDir && echo "Compiled .html placed."
+		fi 
+	fi  
+	mega-logout > /dev/null && let COUNTER=COUNTER+1 && clear && sleep 900s
+done
